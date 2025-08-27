57ef9e148fc83b5466e677e651bfb067bac00827
DESKTOP-D2GC4C5\yulis <yulissa.litzy@gmail.com>
2025-08-27 11:53:49 -0600

    inicio de proyecto

---
+++ views/auth/mensaje.php
@@ -0,0 +1,5 @@
+<main class="auth">
+    <h2 class="auth__heading"><?php echo $titulo; ?></h2>
+    <p class="auth__texto">Es necesario confirmat tu cuenta, revisa tu bandeja de entreda para hacerlo.</p>
+
+</main>
---
+++ views/templates/header.php
@@ -0,0 +1,26 @@
+<header class="header">
+    <div class="header__navegacion">
+        <a href="/registro" class="header__enlace">Registro</a>
+        <a href="/login" class="header__enlace">Iniciar Sesisón</a>
+    </div>
+
+    <div class="header__contenido">
+        <a href="/" class="header__logo"><h1>&#60;DevWebCamp /></h1></a>
+        <p class="header__texto">Octubre 5-6 - 2025</p>
+        <p class="header__texto header__texto--modalidad">En Línea - Prescencial</p>
+
+        <a href="/registro" class="header__boton">Comprar Pase</a>
+    </div>
+</header>
+
+<div class="barra">
+    <div class="barra__contenido">
+        <a href="/"><h2 class="barra__logo">&#60;DevWebCamp /></h2></a>
+        <nav class="navegacion">
+            <a href="/devwepcamp" class="navegacion__enlace">Evento</a>
+            <a href="/paquetes" class="navegacion__enlace">Paquetes</a>
+            <a href="/workshop-conferencias" class="navegacion__enlace">Workshops / Conferencias</a>
+            <a href="/registro" class="navegacion__enlace">Comprar Pase</a>
+        </nav>
+    </div>
+</div>
